10ee2cf20cc0595a5c8d0bd274b790da43728499
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-02-26 14:15:54 +0100

    Creating license models

--- prototyping/models.py
+++ prototyping/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class LicenseManagementDse(models.Model):
-    customer_id = models.AutoField(primary_key=True, verbose_name=_("Customer ID"))
-    name = models.CharField(max_length=255, verbose_name=_("Name"))
-    email = models.EmailField(verbose_name=_("Email"))
-    address = models.CharField(max_length=255, verbose_name=_("Address"), null=True, blank=True)
-    phone = models.CharField(max_length=20, verbose_name=_("Telephone"), null=True, blank=True)
-    license_code = models.CharField(max_length=100, unique=True, verbose_name=_("License Code"))
-    is_active = models.BooleanField(default=True, verbose_name=_("Active"))
-    start_date = models.DateField(verbose_name=_("Start Date"))
-    expiration_date = models.DateField(verbose_name=_("Expiration Date"))
+    customer_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name=_("Nome"))
+    email = models.EmailField(unique=True, verbose_name=_("Email"))
+    address = models.CharField(max_length=255, verbose_name=_("Indirizzo"), null=True, blank=True)
+    tel = models.CharField(max_length=20, verbose_name=_("Telefono"), null=True, blank=True)
+    license_code = models.CharField(max_length=100, unique=True, verbose_name=_("Codice Licenza"))
+    active = models.BooleanField(default=True, verbose_name=_("Attivo"))
+    start_date = models.DateField(verbose_name=_("Data Inizio"))
+    end_date = models.DateField(verbose_name=_("Data Scadenza"))
 
     class Meta:
-        verbose_name = _("License Management DSE")
-        verbose_name_plural = _("Licenses Management DSE")
+        verbose_name = _("Gestione Licenza DSE")
+        verbose_name_plural = _("Gestione Licenze DSE")
 
     def __str__(self):
         return self.name
