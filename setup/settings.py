f2246470f79bc7710fadd236713072405ecf177e
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-03-08 17:42:16 +0100

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "ninja_put_patch_file_upload_middleware.middlewares.process_put_patch",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
