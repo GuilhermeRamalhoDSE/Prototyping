4d5256f10df6a59887b61ce341f7270ebf5a7b77
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-03-13 10:48:14 +0100

    Adjust for deployment

--- setup/settings.py
+++ setup/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.210.220.169', 'gruppodse.org']
 
 
 # Application definition
@@ -143,4 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
+    "https://gruppodse.org/"
 ]
