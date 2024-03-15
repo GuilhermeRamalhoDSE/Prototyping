3057802aefcb774668e1507d6941abca2d5a606a
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-03-15 15:20:59 +0100

    Update auth.py

--- prototyping/auth.py
+++ prototyping/auth.py
@@ -5,39 +5,47 @@ import jwt
 from django.conf import settings
 import logging
 
+# Obtém o modelo de usuário padrão do Django
 User = get_user_model()
+
+# Configura o logger para este módulo
 logger = logging.getLogger(__name__)
 
 class JWTAuth(HttpBearer):
     def authenticate(self, request, token=None):
-        def test_authenticate(token):
-            try:
-                # Simule a lógica de decodificação e autenticação aqui
-                 logger.info("Token:", token)
-                # Insira aqui a lógica de decodificação do token
-            except Exception as e:
-                 logger.info("Erro durante a autenticação:", e)
-
         logger.info("Iniciando autenticação JWT...")
+
+        # Se um token não foi fornecido diretamente, tenta obter da query string
         if token is None:
             token = request.GET.get('token')
-            logger.info(f"Token obtido da query string: {token}")
-        if not token:
-            logger.error("Token não fornecido.")
-            raise HttpError(401, "Authentication token not provided")
+            if token:
+                logger.info(f"Token obtido da query string: {token}")
+            else:
+                logger.error("Token não fornecido.")
+                raise HttpError(401, "Authentication token not provided")
 
         try:
+            # Decodifica o token usando a chave secreta e o algoritmo HS256
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
             user_email = payload.get("email")
+
+            # Tenta obter o usuário pelo e-mail fornecido no payload do token
             user = User.objects.get(email=user_email)
 
+            # Verifica se a flag de superusuário no token corresponde à do usuário
             if payload.get("is_superuser", False) != user.is_superuser:
+                logger.error("Acesso negado.")
                 raise HttpError(403, "Access denied")
 
+            # Se tudo estiver correto, retorna o usuário autenticado
             return user
+
         except jwt.ExpiredSignatureError:
+            logger.error("Token expirado.")
             raise HttpError(401, "Token has expired")
         except jwt.DecodeError:
+            logger.error("Erro ao decodificar o token.")
             raise HttpError(401, "Error decoding token")
         except User.DoesNotExist:
+            logger.error("Usuário não existe.")
             raise HttpError(401, "User does not exist")
