23717621ae49b7967195699910401b301e679aeb
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-03-15 16:52:41 +0100

    Adjusting to receive the token in the request

--- prototyping/auth.py
+++ prototyping/auth.py
@@ -1,38 +1,43 @@
-from ninja.security import HttpBearer
+from ninja.security import HttpBearer, APIKeyQuery
 from ninja.errors import HttpError
-from django.contrib.auth import get_user_model
 import jwt
 from django.conf import settings
-
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class JWTHeaderAuth(HttpBearer):
+    def authenticate(self, request, token):
+        return self.verify_jwt_token(token)
 
-class JWTAuth(HttpBearer):
-    def authenticate(self, request, token=None):
-        if token is None:
-            token = request.headers.get('Authorization')
-            if token and token.startswith("Bearer "):
-                token = token[7:]  
-        
-        if token is None:
-            token = request.GET.get('token')
-        
-        if not token:
-            raise HttpError(401, "Authentication token not provided")
-        
+    def verify_jwt_token(self, token):
         try:
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-            user_email = payload.get("email")
-            user = User.objects.get(email=user_email)
-            
-            if payload.get("is_superuser", False) != user.is_superuser:
-                raise HttpError(403, "Access denied")
-            
+            return User.objects.get(email=payload.get("email"))
+        except Exception as e:
+            raise HttpError(401, "Invalid token or user does not exist")
+
+class JWTQueryAuth(APIKeyQuery):
+    param_name = "token"  
+    def authenticate(self, request, key):
+        return self.verify_jwt_token(key)
+
+    def verify_jwt_token(self, token):
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+            return User.objects.get(email=payload.get("email"))
+        except Exception as e:
+            raise HttpError(401, "Invalid token or user does not exist")
+
+class CombinedAuth:
+    def __init__(self):
+        self.header_auth = JWTHeaderAuth()
+        self.query_auth = JWTQueryAuth()
+
+    def authenticate(self, request):
+        token = request.headers.get('Authorization', '').split('Bearer ')[-1]
+        user = self.header_auth.authenticate(request, token)
+        if user:
             return user
-        except jwt.ExpiredSignatureError:
-            raise HttpError(401, "Token has expired")
-        except jwt.DecodeError:
-            raise HttpError(401, "Error decoding token")
-        except User.DoesNotExist:
-            raise HttpError(401, "User does not exist")
+        token = request.GET.get('token', '')
+        return self.query_auth.authenticate(request, token)
