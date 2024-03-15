20085d0a92253a979f726070d2014c1e4937770f
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-03-15 11:22:23 +0100

    Adjusting login to receive in query

--- prototyping/router/login_route.py
+++ prototyping/router/login_route.py
@@ -5,26 +5,34 @@ from datetime import datetime, timedelta
 from ninja import Router
 from ninja.errors import HttpError
 from prototyping.schemas.login_schema import AuthSchema, LoginResponseSchema
+from ninja import Query
+
 
 login_router = Router(tags=['Login'])
 
 @login_router.post("/", response=LoginResponseSchema)
-def login(request, auth_data: AuthSchema):
-    user = authenticate(email=auth_data.email, password=auth_data.password)
-    if user:
-        expiration_time = datetime.utcnow() + timedelta(days=1)
-        token = jwt.encode({
-            'email': user.email,
-            'exp': expiration_time,
-            'is_superuser': user.is_superuser,
-            'license_id': getattr(user, 'license_id', None) if not user.is_superuser else None,
-        }, settings.SECRET_KEY, algorithm='HS256')
+def login(request, auth_data: AuthSchema = None, email: str = Query(None), password: str = Query(None)):
+    if auth_data is None and email and password:
+        auth_data = AuthSchema(email=email, password=password)
 
-        return {
-            "token": token,
-            "is_superuser": user.is_superuser,
-            "is_staff": user.is_staff,
-            "license_id": getattr(user, 'license_id', None) if not user.is_superuser else None,
-        }
+    if auth_data and auth_data.email and auth_data.password:
+        user = authenticate(email=auth_data.email, password=auth_data.password)
+        if user:
+            expiration_time = datetime.utcnow() + timedelta(days=1)
+            token = jwt.encode({
+                'email': user.email,
+                'exp': expiration_time,
+                'is_superuser': user.is_superuser,
+                'license_id': getattr(user, 'license_id', None) if not user.is_superuser else None,
+            }, settings.SECRET_KEY, algorithm='HS256')
+
+            return {
+                "token": token,
+                "is_superuser": user.is_superuser,
+                "is_staff": user.is_staff,
+                "license_id": getattr(user, 'license_id', None) if not user.is_superuser else None,
+            }
+        else:
+            raise HttpError(401, {"error": "Invalid credentials"})
     else:
-        raise HttpError(401, {"error": "Invalid credentials"})
+        raise HttpError(400, {"error": "Authentication data not provided"})
