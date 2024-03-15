9d5cfd748123cc9a86f9c3ab87a7b9deb538beff
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-03-15 16:39:58 +0100

    Adjusting to receive the token in the request

--- prototyping/auth.py
+++ prototyping/auth.py
@@ -10,20 +10,25 @@ User = get_user_model()
 
 class JWTAuth(HttpBearer):
     def authenticate(self, request, token=None):
+        if token is None:
+            token = request.headers.get('Authorization')
+            if token and token.startswith("Bearer "):
+                token = token[7:]  
         
         if token is None:
             token = request.GET.get('token')
+        
         if not token:
             raise HttpError(401, "Authentication token not provided")
-
+        
         try:
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
             user_email = payload.get("email")
             user = User.objects.get(email=user_email)
-
+            
             if payload.get("is_superuser", False) != user.is_superuser:
                 raise HttpError(403, "Access denied")
-
+            
             return user
         except jwt.ExpiredSignatureError:
             raise HttpError(401, "Token has expired")
--- setup/settings.py
+++ setup/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','52.50.91.105', 'prototypingdse.it', 'www.prototypingdse.it']
 # ALLOWED_HOSTS = []
