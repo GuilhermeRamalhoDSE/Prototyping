5452eafd448a70e4335bfc7a8a5908bdb865ce13
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-03-15 12:30:11 +0100

    Adjusting to receive the token in the request

--- setup/settings.py
+++ setup/settings.py
@@ -151,3 +151,22 @@ CORS_ALLOWED_ORIGINS = [
     'http://prototypingdse.it/',
     'https://prototypingdse.it/'
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
