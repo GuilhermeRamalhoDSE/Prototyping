3b22eeab6862cd4456bd88f5999e4373f8af8fce
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-03-15 17:03:38 +0100

    Adjusting to receive the token in the request

--- prototyping/auth.py
+++ prototyping/auth.py
@@ -1,31 +1,33 @@
-from ninja.security import HttpBearer, APIKeyQuery
+from ninja.security import HttpBearer
 from ninja.errors import HttpError
+from django.contrib.auth import get_user_model
 import jwt
 from django.conf import settings
-from django.contrib.auth import get_user_model
+
 
 User = get_user_model()
 
-class JWTHeaderAuth(HttpBearer):
-    def authenticate(self, request, token):
-        return self.verify_jwt_token(token)
 
-    def verify_jwt_token(self, token):
+class JWTAuth(HttpBearer):
+    def authenticate(self, request, token=None):
+        
+        if token is None:
+            token = request.GET.get('token')
+        if not token:
+            raise HttpError(401, "Authentication token not provided")
+
         try:
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-            return User.objects.get(email=payload.get("email"))
-        except Exception as e:
-            raise HttpError(401, "Invalid token or user does not exist")
+            user_email = payload.get("email")
+            user = User.objects.get(email=user_email)
 
-class JWTQueryAuth(APIKeyQuery):
-    param_name = "token"  
-    def authenticate(self, request, key):
-        return self.verify_jwt_token(key)
+            if payload.get("is_superuser", False) != user.is_superuser:
+                raise HttpError(403, "Access denied")
 
-    def verify_jwt_token(self, token):
-        try:
-            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-            return User.objects.get(email=payload.get("email"))
-        except Exception as e:
-            raise HttpError(401, "Invalid token or user does not exist")
-
+            return user
+        except jwt.ExpiredSignatureError:
+            raise HttpError(401, "Token has expired")
+        except jwt.DecodeError:
+            raise HttpError(401, "Error decoding token")
+        except User.DoesNotExist:
+            raise HttpError(401, "User does not exist")
